4977a823e961ede57c47cd22c78cb2f2648ff34c
leopark9497 <46334602+leopark9497@users.noreply.github.com>
2019-05-14 22:34:37 +0300

    Basic Search Box
    Search Box with Google Place API
    
    Two const "longtitude" and "latitude" assigned with the exact value of searched place's longtitude and latitude

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const searchElement = document.querySelector('[data-city-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', ()=> {
+   const place = searchBox.getPlaces()[0]
+   if (place == null) {return} else {
+     const latitude = place.geometry.location.lat();
+     const longtitude = place.geometry.location.lng();
+   }
+
+})
